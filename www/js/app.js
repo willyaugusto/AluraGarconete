5f00ee402b47b895ff747c42e0c5c6840211684f
willyaugusto <willyaugusto@gmail.com>
2017-04-15 12:20:15 -0300

    Aula 5 - Ok

---
+++ www/js/app.js
@@ -0,0 +1,44 @@
+$('.collection-item').on('click', function(){
+
+    var $badge = $('.badge', this);
+
+    if ($badge.length == 0){
+        $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+    }
+
+    $badge.text(parseInt($badge.text()) + 1);
+
+});
+
+$('badge').on('click', function(){
+    var nomeProduto = this.firstChild.textContent;
+    Materialize.toast(nomeProduto + ' adicionado', 1000);
+});
+
+$('#confirmar').on('click', function(){
+    
+    var texto = '';
+
+    $('.badge').parent().each(function(){
+        var produto = this.firstChild.textContent;
+        Materialize.toast(produto + ' adicionado', 1000);
+
+        var quantidade = this.lastChild.textContent;
+
+        texto += produto + ": " + quantidade + ", ";
+    });
+
+    $('#resumo').text(texto);
+});
+
+$('.modal-trigger').leanModal();
+
+$('.collection').on('click', '.badge', function(){
+    $(this).remove();
+    return false;
+});
+
+$('.acao-limpar').on('click', function(){
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
